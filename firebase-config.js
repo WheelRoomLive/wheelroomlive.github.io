13474962d7874e893464685fcc9094bb50522742
WheelRoomLive <ashenwitch95@gmail.com>
2026-05-23 17:27:41 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,25 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+    apiKey: "AIzaSyD-UCAyg9tPJGkU9GWED_P8Ur-kMhbBBh8",
+    authDomain: "wheel-44c7f.firebaseapp.com",
+    databaseURL: "https://wheel-44c7f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wheel-44c7f",
+    storageBucket: "wheel-44c7f.firebasestorage.app",
+    messagingSenderId: "334213255427",
+    appId: "1:334213255427:web:4d19324cb63f5c2bb29936"
 };
 
-export const ADMIN_UID = "PASTE_ADMIN_FIREBASE_AUTH_UID_HERE";
+export const ADMIN_UID = "HvvWHpuvDAh85fZCPAe5H5fGGzX2";
 export const APP_CHECK_SITE_KEY = "";
 export const ROOM_TTL_MS = 24 * 60 * 60 * 1000;
 
 export function isFirebaseConfigured() {
-  return !firebaseConfig.apiKey.includes("PASTE_") && !firebaseConfig.databaseURL.includes("PASTE_");
+    return !firebaseConfig.apiKey.includes("PASTE_") && !firebaseConfig.databaseURL.includes("PASTE_");
 }
 
 export function isAdminConfigured() {
-  return Boolean(ADMIN_UID) && !ADMIN_UID.includes("PASTE_");
+    return Boolean(ADMIN_UID) && !ADMIN_UID.includes("PASTE_");
 }
 
 export function isAppCheckConfigured() {
-  return Boolean(APP_CHECK_SITE_KEY) && !APP_CHECK_SITE_KEY.includes("PASTE_");
+    return Boolean(APP_CHECK_SITE_KEY) && !APP_CHECK_SITE_KEY.includes("PASTE_");
 }
